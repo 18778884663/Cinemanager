ff65e66d5674acea77c51a66c3df33826035291c
tangxiaohui11 <1971459062@qq.com>
2019-03-27 12:10:32 +0800

    列表视图显示              2019-3-27 第五节

--- app/src/main/java/net/lzzy/cinemanager/fragments/OrdersFragment.java
+++ app/src/main/java/net/lzzy/cinemanager/fragments/OrdersFragment.java
@@ -1,13 +1,6 @@
 package net.lzzy.cinemanager.fragments;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
@@ -15,14 +8,17 @@ import net.lzzy.cinemanager.R;
  * Created by lzzy_gxy on 2019/3/26.
  * Description:
  */
-public class OrdersFragment extends Fragment {
-    public OrdersFragment(){}
+public class OrdersFragment extends BaseFragment {
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_orders,null);
+    protected void populate() {
+        TextView tv=find(R.id.fragment_cinemas_tv);
 
-        return view;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_orders;
     }
 }
